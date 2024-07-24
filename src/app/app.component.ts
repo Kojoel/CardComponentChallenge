588bea2adaba1af159c27f084e98f3fbd2f079c8
Kojoel <nanaabruquah1s@gmail.com>
2024-07-24 09:46:33 +0000

    Added profile card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EcommerceProductCardComponent } from "./ecommerce-product-card/ecommerce-product-card.component";
 import { NewsArticleCardComponent } from "./news-article-card/news-article-card.component";
+import { ProfileCardComponent } from "./profile-card/profile-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EcommerceProductCardComponent, NewsArticleCardComponent],
+  imports: [RouterOutlet, EcommerceProductCardComponent, NewsArticleCardComponent, ProfileCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
