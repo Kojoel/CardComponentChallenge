691f78a8a02d4c81d2868867b19e6f547a28bd06
Kojoel <nanaabruquah1s@gmail.com>
2024-07-24 10:03:19 +0000

    Event card complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { EcommerceProductCardComponent } from "./ecommerce-product-card/ecommerce-product-card.component";
 import { NewsArticleCardComponent } from "./news-article-card/news-article-card.component";
 import { ProfileCardComponent } from "./profile-card/profile-card.component";
+import { EventCardComponent } from "./event-card/event-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EcommerceProductCardComponent, NewsArticleCardComponent, ProfileCardComponent],
+  imports: [RouterOutlet, EcommerceProductCardComponent, NewsArticleCardComponent, ProfileCardComponent, EventCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
